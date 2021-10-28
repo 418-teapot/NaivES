9b8b3eeada4ff7871fe7b2363fd98c0ad48d990b
418teapot <wangshuo2912@foxmail.com>
2021-10-28 20:31:09 +0800

    refactor(cpu): refactor opcode

--- src/cpu/opcode.h
+++ src/cpu/opcode.h
@@ -3,108 +3,62 @@
 
 namespace cpu {
 
-enum opcode {
-  ADC_IMM = 0x69,
-  ADC_0P = 0x65,
-  ADC_IDX_0P_X = 0x75,
-  ADC_ABS = 0x6D,
-  ADC_IDX_ABS_X = 0x7D,
-  ADC_IDX_ABS_Y = 0x79,
-  ADC_IDX_IND = 0x61,
-  ADC_IND_IDX = 0x71,
+enum class Op1 {
+  ADC = 0x60,
+  SBC = 0xE0,
+  AND = 0x20,
+  EOR = 0x40,
+  ORA = 0x00,
+  CMP = 0xC0,
+  LDA = 0xA0,
+  STA = 0x80
+};
 
-  INC_0P = 0xE6,
-  INC_IDX_0P_X = 0xF6,
-  INC_ABS = 0xEE,
-  INC_IDX_ABS_X = 0xFE,
+enum class AddrMode1 {
+  IMME = 0x09, // STA doesn't have this mode
+  ZERO_PAGE = 0x05,
+  IDX_ZERO_PAGE_X = 0x15,
+  ABSOLUTE = 0x0D,
+  IDX_ABS_X = 0x1D,
+  IDX_ABS_Y = 0x19,
+  IDX_IND = 0x01,
+  IND_IDX = 0x11
+};
 
+enum class Op2 {
+  ASL = 0x00,
+  LSR = 0x40,
+  ROL = 0x20,
+  ROR = 0x60,
+  INC = 0xE0,
+  DEC = 0xC0
+};
+
+enum class AddrMode2 {
+  ACC = 0x0A, // INC, DEC don't have this mode
+  ZERO_PAGE = 0x06,
+  IDX_ZERO_PAGE_X = 0x16,
+  ABSOLUTE = 0x0E,
+  IDX_ABSOLUTE_X = 0x1E
+};
+
+enum class OpBranch {
+  BCC = 0x90,
+  BCS = 0xB0,
+  BEQ = 0xF0,
+  BMI = 0x30,
+  BNE = 0xD0,
+  BPL = 0x10,
+  BVC = 0x50,
+  BVS = 0x70,
+};
+
+enum class OpImpl {
   INX = 0xE8,
   INY = 0xC8,
-
-  SBC_IMM = 0xE9,
-  SBC_0P = 0xE5,
-  SBC_IDX_0P_X = 0xF5,
-  SBC_ABS = 0xED,
-  SBC_IDX_ABS_X = 0xFD,
-  SBC_IDX_ABS_Y = 0xF9,
-  SBC_IDX_IND = 0xE1,
-  SBC_IND_IDX = 0xF1,
-
-  DEC_0P = 0xC6,
-  DEC_IDX_0P_X = 0xD6,
-  DEC_ABS = 0xCE,
-  DEX_IDX_ABS_X = 0xDE,
-
   DEX = 0xCA,
   DEY = 0x88,
-
-  AND_IMM = 0x29,
-  AND_0P = 0x25,
-  AND_IDX_0P_X = 0x35,
-  AND_ABS = 0x2D,
-  AND_IDX_ABS_X = 0x3D,
-  AND_IDX_ABS_Y = 0x39,
-  AND_IDX_IND = 0x21,
-  AND_IND_IDX = 0x31,
-
-  EOR_IMM = 0x49,
-  EOR_0P = 0x45,
-  EOR_IDX_0P_X = 0x55,
-  EOR_ABS = 0x4D,
-  EOR_IDX_ABS_X = 0x5D,
-  EOR_IDX_ABS_Y = 0x59,
-  EOR_IDX_IND = 0x41,
-  EOR_IND_IDX = 0x51,
-  
-  ORA_IMM = 0x09,
-  ORA_0P = 0x05,
-  ORA_IDX_0P_X = 0x15,
-  ORA_ABS = 0x0D,
-  ORA_IDX_ABS_X = 0x1D,
-  ORA_IDX_ABS_Y = 0x19,
-  ORA_IDX_IND = 0x01,
-  ORA_IND_IDX = 0x11,
-
-  BIT_0P = 0x24,
-  BIT_ABS = 0x2C,
-
-  ASL_ACC = 0x0A,
-  ASL_0P = 0x06,
-  ASL_IDX_0P_X = 0x16,
-  ASL_ABS = 0x0E,
-  ASL_IDX_ABS_X = 0x1E,
-
-  LSR_ACC = 0x4A,
-  LSR_0P = 0x46,
-  LSR_IDX_0P_X = 0x56,
-  LSR_ABS = 0x4E,
-  LSR_IDX_ABS_X = 0x5E,
-
-  ROL_ACC = 0x2A,
-  ROL_0P = 0x26,
-  ROL_IDX_0P_X = 0x36,
-  ROL_ABS = 0x2E,
-  ROL_IDX_ABS_X = 0x4E,
-
-  ROR_ACC = 0x6A,
-  ROR_0P = 0x66,
-  ROR_IDX_0P_X = 0x76,
-  ROR_ABS = 0x6E,
-  ROR_IDX_ABS_X = 0x7E,
-
-  BCC_REL = 0x90,
-  BCS_REL = 0xB0,
-  BEQ_REL = 0xF0,
-  BMI_REL = 0x30,
-  BNE_REL = 0xD0,
-  BPL_REL = 0x10,
-  BVC_REL = 0x50,
-  BVS_REL = 0x70,
-
   BRK = 0x00,
-  JMP_ABS = 0x4C,
-  JMP_IND = 0x6C,
-  JSR_ABS = 0x20,
   RTI = 0x40,
   RTS = 0x60,
   CLC = 0x18,
@@ -114,59 +68,6 @@ enum opcode {
   SEC = 0x38,
   SED = 0xF8,
   SEI = 0x78,
-
-  CMP_IMM = 0x0C9,
-  CMP_0P = 0xC5,
-  CMP_IDX_0P_X = 0xD5,
-  CMP_ABS = 0xCD,
-  CMP_IDX_ABS_X = 0xDD,
-  CMP_IDX_ABS_Y = 0xD9,
-  CMP_IDX_IND = 0xC1,
-  CMP_IND_IDX = 0xD1,
-
-  CPX_IMM = 0xE0,
-  CPX_0P = 0xE4,
-  CPX_ABS = 0xEC,
-  CPY_IMM = 0xC0,
-  CPY_0P = 0xC4,
-  CPY_ABS = 0xCC,
-
-  LDA_IMM = 0xA9,
-  LDA_0P = 0xA5,
-  LDA_IDX_0P_X = 0xB5,
-  LDA_ABS = 0xAD,
-  LDA_IDX_ABS_X = 0xBD,
-  LDA_IDX_ABS_Y = 0xB9,
-  LDA_IDX_IND = 0xA1,
-  LDA_IND_IDX = 0xB1,
-
-  LDX_IMM = 0xA2,
-  LDX_0P = 0xA6,
-  LDX_IDX_0P_Y = 0xB6,
-  LDX_ABS = 0xAE,
-  LDX_IDX_ABS_Y = 0xBE,
-
-  LDY_IMM = 0xA0,
-  LDY_0P = 0xA4,
-  LDY_IDX_0P_X = 0xB4,
-  LDY_ABS = 0xAC,
-  LDY_IDX_ABS_X = 0xBC,
-
-  STA_0P = 0x85,
-  STA_IDX_0P_X = 0x95,
-  STA_ABS = 0x8D,
-  STA_IDX_ABS_X = 0x9D,
-  STA_IDX_ABS_Y = 0x99,
-  STA_IDX_IND = 0x81,
-  STA_IND_IDX = 0x91,
-
-  STX_0P = 0x86,
-  STX_IDX_0P_Y = 0x96,
-  STX_ABS = 0x8E,
-  STY_0P = 0x84,
-  STY_IDX_0P_X = 0x94,
-  STY_ABS = 0x8C,
-
   TAX = 0xAA,
   TAY = 0xA8,
   TSX = 0xBA,
@@ -177,10 +78,38 @@ enum opcode {
   PHP = 0x08,
   PLA = 0x68,
   PLP = 0x28,
-  NOP = 0xEA,
-
-  UNDEF = 0xFF
+  NOP = 0xEA
 };
+
+enum class OpXY {
+  CPX = 0xE0,
+  STX = 0x80,
+  LDX = 0xA2,
+  CPY = 0xC0,
+  STY = 0x80,
+  LDY = 0xA0,
+};
+
+enum class AddrModeXY {
+  IMME = 0x00, // STX, STY don't have this mode
+  ZERO_PAGE = 0x04,
+  IDX_ZERO_PAGE = 0x14, // CPX, CPY don't support this mode
+  ABSOLUTE = 0x0C,
+  IDX_ABS = 0x1C // only LDX and LDY
+};
+
+enum class OpOthers {
+  BIT = 0x20,
+  JMP = 0x40,
+  JSR = 0x20
+};
+
+enum class AddrModeOthers {
+  ZERO_PAGE = 0x04, // only BIT
+  ABSOLUTE = 0x0C, // JSR's absolute is 0x00
+  INDIRECT = 0x2C // only JMP
+};
+
 }; // namespace cpu
 
 #endif // CPU_OPCODE_H_
