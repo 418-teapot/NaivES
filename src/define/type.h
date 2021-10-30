c2a4921ffa5b9d599b334e1d256b9d5d302375bf
418teapot <wangshuo2912@foxmail.com>
2021-10-30 22:01:02 +0800

    feat(define): add some define

---
+++ src/define/type.h
@@ -0,0 +1,18 @@
+#ifndef DEFINE_TYPE_H_
+#define DEFINE_TYPE_H_
+
+#include <cstdint>
+
+#define IN
+#define OUT
+
+typedef uint8_t Imme;
+typedef uint8_t ZeroPageAddr;
+typedef uint8_t IdxZeroPageAddr;
+typedef uint16_t AbsAddr;
+typedef uint16_t IdxAbsAddrX;
+typedef uint16_t IdxAbsAddrY;
+typedef uint8_t IndIdxAddr;
+typedef uint8_t IdxIndAddr;
+
+#endif // DEFINE_TYPE_H_
